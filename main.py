8dd863af38febb21726a15c2363085e6ba3bfb70
Donglan Liang <donglan.digital@hotmail.com>
2025-05-29 16:09:48 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+# Dictionary representing the morse code chart
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+user_input_text = input(
+    'Type in the text you want to converter into Morse Code: ').upper()
+
+def generate_morse_code ():
+    morse_code = ""
+
+    for char in user_input_text:
+        if char in MORSE_CODE_DICT:
+            code = MORSE_CODE_DICT[char]
+            morse_code += code
+
+        # mark unknown characters
+        else:
+            morse_code += "? "
+
+        return morse_code
+print(f"Your Morse Code: {generate_morse_code()}")
+        
+
+    
